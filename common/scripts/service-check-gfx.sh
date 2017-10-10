a2a853be927de72bf582efb8da256159f5aeaf68
Jan-Simon Möller <jsmoeller@linuxfoundation.org>
2017-10-10 23:06:21 +0200

    Prepare the nogfx variant of the service check
    and enhance verbosity.
    
    Change-Id: Ia95a6236ffc4229703714ad58b70b21c00884c5c
    Signed-off-by: Jan-Simon Möller <jsmoeller@linuxfoundation.org>

--- common/scripts/service-check-gfx.sh
+++ common/scripts/service-check-gfx.sh
@@ -7,16 +7,19 @@ ALL="${REQUIREDSOCKETS} ${REQUIREDSERVICES}"
 RESULT="unknown"
 
 for i in ${ALL} ; do
-    echo "########## Test for service ${i} being active ##########"
+    echo -e "\n########## Test for service ${i} being active ##########\n"
     systemctl is-active ${i} >/dev/null 2>&1
     if [ $? -eq 0 ] ; then
         RESULT="pass"
     else
-        systemctl status ${i} || true
         RESULT="fail"
     fi
-    echo "########## Result for service ${i} : $RESULT ##########"
     lava-test-case ${i} --result ${RESULT}
+
+    if [ x"fail" == x"${RESULT}" ] ; then
+        systemctl status ${i} || true
+    fi
+    echo -e "\n########## Result for service ${i} : $RESULT ##########\n"
 done
 
 exit 0
---
+++ common/scripts/service-check-nogfx.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+REQUIREDSOCKETS="cynara.socket security-manager.socket"
+REQUIREDSERVICES="afm-system-daemon.service afm-user-daemon.service connman.service"
+
+ALL="${REQUIREDSOCKETS} ${REQUIREDSERVICES}"
+RESULT="unknown"
+
+for i in ${ALL} ; do
+    echo -e "\n########## Test for service ${i} being active ##########\n"
+    systemctl is-active ${i} >/dev/null 2>&1
+    if [ $? -eq 0 ] ; then
+        RESULT="pass"
+    else
+        RESULT="fail"
+    fi
+    lava-test-case ${i} --result ${RESULT}
+
+    if [ x"fail" == x"${RESULT}" ] ; then
+        systemctl status ${i} || true
+    fi
+    echo -e "\n########## Result for service ${i} : $RESULT ##########\n"
+done
+
+exit 0

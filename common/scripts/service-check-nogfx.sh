84870c22625876a5ecc369eef21b18597d967571
Jan-Simon Möller <jsmoeller@linuxfoundation.org>
2017-10-12 11:56:15 +0200

    Adapt service checks for gfx and nogfx case
    Based on SPEC-949, remove the afm-user-daemon service as it is a session service and no system service.
    Add systemctl list-units at the end.
    
    Bug-AGL: SPEC-949
    
    Change-Id: I90cde515efc5dbbd031691026f76345f8828be1a
    Signed-off-by: Jan-Simon Möller <jsmoeller@linuxfoundation.org>

--- common/scripts/service-check-nogfx.sh
+++ common/scripts/service-check-nogfx.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
-REQUIREDSOCKETS="cynara.socket security-manager.socket"
-REQUIREDSERVICES="afm-system-daemon.service afm-user-daemon.service connman.service"
+export LANG=C
+export TERM=dumb
+
+REQUIREDSOCKETS="cynara.socket dbus.socket security-manager.socket"
+REQUIREDSERVICES="afm-system-daemon.service connman.service ofono.service bluetooth.service"
 
 ALL="${REQUIREDSOCKETS} ${REQUIREDSERVICES}"
 RESULT="unknown"
 
 for i in ${ALL} ; do
-    echo -e "\n########## Test for service ${i} being active ##########\n"
+    echo -e "\n\n########## Test for service ${i} being active ##########\n\n"
+
     systemctl is-active ${i} >/dev/null 2>&1
     if [ $? -eq 0 ] ; then
         RESULT="pass"
     else
         RESULT="fail"
     fi
-    lava-test-case ${i} --result ${RESULT}
 
-    if [ x"fail" == x"${RESULT}" ] ; then
-        systemctl status ${i} || true
-    fi
-    echo -e "\n########## Result for service ${i} : $RESULT ##########\n"
+    lava-test-case ${i} --result ${RESULT}
+    systemctl status ${i} || true
+    echo -e "\n\n"
+
+    echo -e "\n\n########## Result for service ${i} : $RESULT ##########\n\n"
 done
+
+
+echo "------------------------------------------------"
+echo "All systemd units:"
+echo "------------------------------------------------"
+systemctl list-units || true
+echo "------------------------------------------------"
+echo "Only the failed systemd units:"
+echo "------------------------------------------------"
+( systemctl list-units | grep failed ) || true
+
 
 exit 0
